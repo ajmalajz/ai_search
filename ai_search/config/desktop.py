39391485a53147449a73b474409127fcc6044ddf
ajmalajz <136721858+ajmalajz@users.noreply.github.com>
2025-11-29 22:00:28 +0300

    Create desktop.py

---
+++ ai_search/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "AI Search",
+            "color": "blue",
+            "icon": "octicon octicon-search",
+            "type": "module",
+            "label": _("AI Search")
+        }
+    ]
