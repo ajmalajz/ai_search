512d013a8d2341bcd7b8dfa7d1a8c09fa9e55191
ajmalajz <136721858+ajmalajz@users.noreply.github.com>
2025-12-01 20:03:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-
+# setup.py
 from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
 
 setup(
     name='ai_search',
-    version='1.0.0',
-    description='AI-like Arabic & English search for ERPNext using Meilisearch',
-    author='Your Company',
-    license='MIT',
+    version='0.0.1',
+    description='Custom app for fuzzy search functionality in ERPNext.',
+    author='Your Name',
     packages=find_packages(),
+    zip_safe=False,
     include_package_data=True,
-    install_requires=['requests'],
+    install_requires=install_requires
 )
